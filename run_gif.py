faf37d64baa3b5f043a2ea99b282f756532e9a51
lovetocode14 <82523141+lovetocode14@users.noreply.github.com>
2021-12-19 14:27:23 -0600

    Update run_gif.py

--- run_gif.py
+++ run_gif.py
@@ -3,14 +3,16 @@ import ctypes
 
 # path to folder that contains the .jpg that were converted 
 path = ""
+number_of_frames = 0
+avg_frame_duration = 0
 
 
 
 
 def play_gif():
     while True:
-        for x in range(603):
-            time.sleep(0.057)
+        for x in range(number_of_frames):
+            time.sleep(average_frame_duration)
 
             run_path = path + str(x) + ".jpg"
             ctypes.windll.user32.SystemParametersInfoW(20, 0, run_path, 1)
